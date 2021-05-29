e7dd8ff0411700db0a44f7a21c418de082ee93ef
qyjbeijing2017 <qyjbeijing@163.com>
2021-05-29 16:57:28 +0800

    重新build Android

--- NativeAndroidApp/app/src/main/java/org/qyj/nativeandroidapp/MainActivity.java
+++ NativeAndroidApp/app/src/main/java/org/qyj/nativeandroidapp/MainActivity.java
@@ -2,11 +2,7 @@ package org.qyj.nativeandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import com.unity3d.player.UnityPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void OnUnityStart(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
-
-        startActivity(intent);
     }
 }
