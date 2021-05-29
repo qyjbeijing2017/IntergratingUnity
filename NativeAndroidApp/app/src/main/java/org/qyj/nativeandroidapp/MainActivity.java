e73b48bffb3fb6faf487e5171d8f9f625b833f47
qyjbeijing2017 <qyjbeijing@163.com>
2021-05-29 19:08:37 +0800

    成功嵌入原生android

--- NativeAndroidApp/app/src/main/java/org/qyj/nativeandroidapp/MainActivity.java
+++ NativeAndroidApp/app/src/main/java/org/qyj/nativeandroidapp/MainActivity.java
@@ -2,13 +2,23 @@ package org.qyj.nativeandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.unity3d.player.UnityPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int m_clickTime = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStartUnity(View view){
+        Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
+        startActivity(intent);
     }
 }
